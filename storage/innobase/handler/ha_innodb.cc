4533435b79243868722fc9570cc1f587aa4e9b1b
Marko Mäkelä <marko.makela@mariadb.com>
2022-06-10 17:57:33 +0300

    MDEV-28766: SET GLOBAL innodb_log_file_buffering
    In commit c4c88307091cb16886562e9e7b77f5fd077d34b5 (MDEV-28111) we disabled
    the file system cache on the InnoDB write-ahead log file (ib_logfile0)
    by default on Linux.
    
    It turns out that under some circumstances, writing to the log via the
    file system cache does improve throughput, especially on slow storage
    or at a small number of concurrent transactions.
    
    On Linux and Microsoft Windows, we will introduce the Boolean global
    variable innodb_log_file_buffering for explicitly enabling or disabling
    the file system cache on the redo log file. On other operating systems,
    no interface has been implemented for controlling the file system cache.
    
    On Microsoft Windows, before and after this change, we will enable
    the file system cache on the log if innodb_flush_log_at_trx_commit=2
    is set on server startup.
    
    When a persistent memory interface is being used for the log,
    the value cannot be changed from innodb_log_file_buffering=OFF.
    On Linux, when the physical block size cannot be determined
    to be a power of 2 between 64 and 4096 bytes, the value cannot be
    changed from innodb_log_file_buffering=ON.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -4063,6 +4063,13 @@ static int innodb_init_params()
 			fprintf(stderr, "InnoDB: using O_DIRECT due to atomic writes.\n");
 		}
 	}
+
+	if (srv_flush_log_at_trx_commit == 2) {
+		/* Compatibility with old releases for Windows:
+		Enable the file system cache if
+		innodb_flush_log_at_trx_commit=2 at startup. */
+		log_sys.log_buffered = true;
+	}
 #endif
 
 	if (srv_read_only_mode) {
@@ -18441,6 +18448,16 @@ buffer_pool_load_abort(
 	}
 }
 
+#if defined __linux__ || defined _WIN32
+static void innodb_log_file_buffering_update(THD *thd, st_mysql_sys_var*,
+                                             void *, const void *save)
+{
+  mysql_mutex_unlock(&LOCK_global_system_variables);
+  log_sys.set_buffered(*static_cast<const my_bool*>(save));
+  mysql_mutex_lock(&LOCK_global_system_variables);
+}
+#endif
+
 /** Update innodb_status_output or innodb_status_output_locks,
 which control InnoDB "status monitor" output to the error log.
 @param[out]	var	current value
@@ -18857,7 +18874,7 @@ static MYSQL_SYSVAR_ENUM(flush_method, srv_file_flush_method,
 
 static MYSQL_SYSVAR_STR(log_group_home_dir, srv_log_group_home_dir,
   PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_READONLY,
-  "Path to InnoDB log files.", NULL, NULL, NULL);
+  "Path to ib_logfile0", NULL, NULL, NULL);
 
 static MYSQL_SYSVAR_DOUBLE(max_dirty_pages_pct, srv_max_buf_pool_modified_pct,
   PLUGIN_VAR_RQCMDARG,
@@ -19248,6 +19265,13 @@ static MYSQL_SYSVAR_SIZE_T(log_buffer_size, log_sys.buf_size,
   PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_READONLY,
   "Redo log buffer size in bytes.",
   NULL, NULL, 16U << 20, 2U << 20, SIZE_T_MAX, 4096);
+
+#if defined __linux__ || defined _WIN32
+static MYSQL_SYSVAR_BOOL(log_file_buffering, log_sys.log_buffered,
+  PLUGIN_VAR_NOCMDARG,
+  "Enable the file system cache for the redo log.",
+  nullptr, innodb_log_file_buffering_update, FALSE);
+#endif
 
 static MYSQL_SYSVAR_ULONGLONG(log_file_size, srv_log_file_size,
   PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_READONLY,
@@ -19691,6 +19715,7 @@ static struct st_mysql_sys_var* innobase_system_variables[]= {
   MYSQL_SYSVAR(deadlock_report),
   MYSQL_SYSVAR(page_size),
   MYSQL_SYSVAR(log_buffer_size),
+  MYSQL_SYSVAR(log_file_buffering),
   MYSQL_SYSVAR(log_file_size),
   MYSQL_SYSVAR(log_group_home_dir),
   MYSQL_SYSVAR(max_dirty_pages_pct),
